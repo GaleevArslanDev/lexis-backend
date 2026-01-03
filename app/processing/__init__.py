0c804ef2c9706c574c821f647d9345f773488486
GaleevArslanDev <115714847+GaleevArslanDev@users.noreply.github.com>
2026-01-03 20:30:11 +0300

    Update __init__.py

--- app/processing/__init__.py
+++ app/processing/__init__.py
@@ -1,14 +1,10 @@
 # Инициализация модулей обработки
-from .image_preprocessor import ImagePreprocessor
-from .ocr_engine import LightweightOCREngine
 from .formula_extractor import FormulaExtractor
 from .solution_analyzer import SolutionAnalyzer
 from .confidence_scorer import ConfidenceScorer
 from .sympy_evaluator import SymPyEvaluator
 
 __all__ = [
-    "ImagePreprocessor",
-    "LightweightOCREngine",
     "FormulaExtractor",
     "SolutionAnalyzer",
     "ConfidenceScorer",
