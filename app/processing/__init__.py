45fbb3ad845a149cf82ef3282ed77fae28e5c8fe
GaleevArslanDev <115714847+GaleevArslanDev@users.noreply.github.com>
2026-01-04 00:57:39 +0300

    fixed

--- app/processing/__init__.py
+++ app/processing/__init__.py
@@ -1,11 +1,9 @@
 # Инициализация модулей обработки
-from .formula_extractor import FormulaExtractor
 from .solution_analyzer import SolutionAnalyzer
 from .confidence_scorer import ConfidenceScorer
 from .sympy_evaluator import SymPyEvaluator
 
 __all__ = [
-    "FormulaExtractor",
     "SolutionAnalyzer",
     "ConfidenceScorer",
     "SymPyEvaluator"
